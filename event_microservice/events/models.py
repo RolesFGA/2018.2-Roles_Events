8ab48c27bf6c943a65a5edf1b8bc29a79d23fcc0
Henrike100 <Henrikem100@gmail.com>
2018-09-27 12:35:42 -0300

    fga-eps-mds/2018.2-Roles#65 Model do evento criado
    Co-authored-by: Henrike100 <henrikem100@gmail.com>
    Co-authored-by: raulvicto <rvictors@hotmail.com>

---
+++ event_microservice/events/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=50)
+    organizer = models.CharField(max_length=50)
+    value = models.FloatField(default= '0.0')
+    amount = models.IntegerField(default= '0')
+    address = models.TextField()
+    foods = models.TextField()
+    drinks = models.TextField()
+    photo = models.ImageField()
