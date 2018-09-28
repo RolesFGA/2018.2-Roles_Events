37823dd2b462bdf916e775e8bbf19683c138cfbc
Henrike100 <Henrikem100@gmail.com>
2018-09-28 11:19:00 -0300

    fga-eps-mds/2018.2-Roles#65 Add atributos na classe Evento
    Co-authored-by: Henrike100 <henrikem100@gmail.com>
    Co-authored-by: raulvicto <rvictors@hotmail.com>

--- event_microservice/events/models.py
+++ event_microservice/events/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 class Event(models.Model):
     event_name = models.CharField(max_length=50)
+    linkReference = models.URLField(max_length=200, default='')
     organizer = models.CharField(max_length=50)
+    organizerTel = models.CharField(max_length=20, default='')
     value = models.FloatField(default= '0.0')
-    amount = models.IntegerField(default= '0')
-    address = models.TextField()
+    address = models.CharField(max_length=50)
+    linkAddress = models.URLField(max_length=200, default='')
+    eventDate = models.DateField(auto_now=False, blank=True)
+    eventHour = models.TimeField(auto_now=False, blank=True)
+    adultOnly = models.BooleanField(default=False)
+    eventDescription = models.TextField()
+    photo = models.ImageField()
     foods = models.TextField()
     drinks = models.TextField()
-    photo = models.ImageField()
 
     def __str__(self):
         return self.event_name
