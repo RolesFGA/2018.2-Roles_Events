b8348558b07a48203f0d88e5bf88cea1449f1b7b
Henrike100 <Henrikem100@gmail.com>
2018-09-28 16:54:30 -0300

    fga-eps-mds/2018.2-Roles#65 Add Autenticação
    Co-authored-by: Henrike100 <henrikem100@gmail.com>
    Co-authored-by: raulvicto <rvictors@hotmail.com>

--- event_microservice/event_microservice/settings.py
+++ event_microservice/event_microservice/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -42,6 +43,23 @@ INSTALLED_APPS = [
     'oauth2_provider',
     'events',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'NON_FIELD_ERRORS_KEY': 'global',
+}
+
+JWT_AUTH = {
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': timedelta(days=2),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- event_microservice/event_microservice/urls.py
+++ event_microservice/event_microservice/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url,include
 from django.urls import path
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('event/', include('events.urls')),
+    path('login/', obtain_jwt_token),
+    path('refresh-token/', refresh_jwt_token),
 ]
