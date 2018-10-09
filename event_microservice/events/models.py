74bbd3f9c0b13dcbf71ec4857cdb655b046ce1bf
Henrike100 <Henrikem100@gmail.com>
2018-10-09 14:37:22 -0300

    fga-eps-mds/2018.2-Roles#88 Ajustando campos necessários para eventos

--- event_microservice/events/models.py
+++ event_microservice/events/models.py
@@ -2,32 +2,31 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from datetime import date
 
+def not_negative(value):
+    if value < 0:
+        raise ValidationError('Numero não pode ser negativo')
+
+def corret_time(value):
+    today = date.today()
+    if value < today:
+        raise ValidationError('Tempo incorreto')
+
 
 class Event(models.Model):
-    def not_negative(value):
-        if value < 0:
-            raise ValidationError('Numero não pode ser negativo')
-
-    def corret_time(value):
-        today = date.today()
-        if value < today:
-            raise ValidationError('Tempo incorreto')
-
-    owner = models.CharField("Criador do Evento", max_length=50)
+    owner = models.CharField("Criador do Evento", max_length=50, default="")
     eventName = models.CharField("Nome do Evento", max_length=50)
-    linkReference = models.URLField("Link de Referência", max_length=200, help_text="Com quem entrar em contato caso necessário")
+    linkReference = models.URLField("Link de Referência", max_length=200, default="")
     organizer = models.CharField("Nome para Contato", max_length=50)
-    organizerTel = models.CharField("Telefone para Contato", max_length=20, help_text="Ex: 61 912345678")
-    value = models.DecimalField("Valor do Ingresso", help_text="Ex: 49.99", max_digits=12, decimal_places=2, validators=[not_negative], default=0.00)
+    value = models.DecimalField("Valor do Ingresso", max_digits=12, decimal_places=2, validators=[not_negative], default=0.00)
     address = models.CharField("Local do Evento", max_length=50)
-    linkAddress = models.URLField("Localização no Google Maps", max_length=200)
+    linkAddress = models.URLField("Localização no Google Maps", max_length=200, default="")
     eventDate = models.DateField("Data do Rolê", auto_now=False, help_text="DD/MM/AAAA", validators=[corret_time])
     eventHour = models.TimeField("Horário do Rolê", auto_now=False)
     adultOnly = models.BooleanField("+18", default=False, help_text="Marque caso seja só para adultos")
     eventDescription = models.TextField("Descrição", help_text="Descrição do evento")
     photo = models.ImageField("Foto", default="")
-    foods = models.TextField("Comidas", help_text="Lista de comidas do evento")
-    drinks = models.TextField("Bebidas", help_text="Lista de bebidas do evento")
+    foods = models.TextField("Comidas")
+    drinks = models.TextField("Bebidas")
 
     class Meta:
         ordering = ('eventDate', 'eventHour', 'eventName',)
