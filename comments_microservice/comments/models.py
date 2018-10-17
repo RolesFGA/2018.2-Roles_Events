1d1bb6974df113fedeb8bb4d6c0ed0760612e46d
Henrike100 <henrikem100@gmail.com>
2018-10-16 22:13:03 -0300

    fga-eps-mds/2018.2-Roles#99 Modelo inicial da classe Comentário

--- comments_microservice/comments/models.py
+++ comments_microservice/comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    author = models.CharField(max_length=128)
+    text = models.CharField(max_length=128)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return self.author
--- comments_microservice/comments/views.py
+++ comments_microservice/comments/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Comment
+from .serializers import CommentSerializer
 
-# Create your views here.
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
