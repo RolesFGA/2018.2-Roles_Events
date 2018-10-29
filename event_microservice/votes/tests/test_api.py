cd19c1f3178e9f582c9abd834a24ba25ef5db3b3
Henrike100 <Henrikem100@gmail.com>
2018-10-29 19:18:21 -0300

    fga-eps-mds/2018.2-Roles#104 Add testes para curtidas de eventos

--- event_microservice/votes/tests/test_api.py
+++ event_microservice/votes/tests/test_api.py
@@ -1,23 +1,35 @@
 __author__ = 'consultadd66'
 
+import sys
+sys.path.append("..")
+
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
-from tixdo.third_party_apps.votes.models import Vote
-from django.core.urlresolvers import reverse
-from tixdo.movies.models import Movies
-from tixdo.users.models import User
+from ..models import Vote
+from rest_framework.reverse import reverse
+from events.models import Event
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+from rest_framework.test import APIClient
 
 
 class CreateVoteTest(APITestCase):
 
     def setUp(self):
+        """
         self.username = 'admin'
         self.password = 'admin'
         self.email = 'admin@gmail.com'
         self.user = User.objects.create_user(self.username, self.email, self.password)
-        self.content_type = ContentType.objects.get(app_label="users", model="user")
+        self.content_type = ContentType.objects.get(model="user")
         self.data = {'content_type': self.content_type.id, 'object_id': 1, 'user': self.user.id}
+        """
+
+        user = User.objects.create(username="User01")
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+        self.content_type = ContentType.objects.get(model="event")
+        self.data = {'content_type': self.content_type.id, 'object_id': 1, 'user': user.id}
 
     def test_can_create_vote(self):
         response = self.client.post(reverse('vote-list'), self.data)
@@ -26,6 +38,10 @@ class CreateVoteTest(APITestCase):
 
 class ReadVoteTest(APITestCase):
     def setUp(self):
+        user = User.objects.create(username="User01")
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+        self.content_type = ContentType.objects.get(model="event")
         self.vote = Vote.objects.create(content_type_id=1, object_id=1, user_id=1)
 
     def test_can_read_vote_list(self):
@@ -39,12 +55,11 @@ class ReadVoteTest(APITestCase):
 
 class UpdateVoteTest(APITestCase):
     def setUp(self):
-        self.username = 'admin'
-        self.password = 'admin'
-        self.email = 'admin@gmail.com'
-        self.user = User.objects.create_user(self.username, self.email, self.password)
-        self.content_type = ContentType.objects.get(app_label="users", model="user")
-        self.data = {'content_type': self.content_type.id, 'object_id': 1, 'user': self.user.id}
+        user = User.objects.create(username="User01")
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+        self.content_type = ContentType.objects.get(model="event")
+        self.data = {'content_type': self.content_type.id, 'object_id': 1, 'user': user.id}
         self.vote = Vote.objects.create(content_type_id=1, object_id=1, user_id=1)
 
     def test_can_update_vote(self):
@@ -54,6 +69,10 @@ class UpdateVoteTest(APITestCase):
 
 class DeleteVoteTest(APITestCase):
     def setUp(self):
+        user = User.objects.create(username="User01")
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
+        self.content_type = ContentType.objects.get(model="event")
         self.vote = Vote.objects.create(content_type_id=1, object_id=1, user_id=1)
 
     def test_can_delete_vote(self):
@@ -63,83 +82,90 @@ class DeleteVoteTest(APITestCase):
 
 class VoteQuerysetTest(APITestCase):
     def setUp(self):
-        self.username = 'admin'
-        self.password = 'admin'
-        self.email = 'admin@gmail.com'
-        self.user = User.objects.create_user(self.username, self.email, self.password)
+        user = User.objects.create(username="User01")
+        self.client = APIClient()
+        self.client.force_authenticate(user=user)
 
-        self.movie = Movies.objects.create(title='movie_name', release_date='2016-01-01')
+        self.event = Event.objects.create(eventName= 'Teste1',
+                                          owner=user,
+                                          eventDate= "2018-12-12",
+                                          eventHour= "03:03:00",
+                                          organizer= "Henrique",
+                                          address= "Here",
+                                          eventDescription= "Chato",
+                                          foods= "Comidas",
+                                          drinks= "Bebidas")
 
     def test_up(self):
         # user should be login to vote
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('vote-list') + 'up/', {'model': 'movies', 'id': str(self.movie.id), 'vote': 'true'})
+
+        response = self.client.get(reverse('vote-list') + 'up/', {'model': 'event', 'id': str(self.event.id), 'vote': 'true'})
         assert response.status_code == 200
         assert response.data['message'] == 'Successfully voted'
 
-        response = self.client.get(reverse('vote-list') + 'up/', {'model': 'movies', 'id': str(self.movie.id)})
+        response = self.client.get(reverse('vote-list') + 'up/', {'model': 'event', 'id': str(self.event.id)})
         assert response.data['message'] == "Please provide a like or dislike parameter."
 
     def test_up_post(self):
         # user should be login to vote
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.post(reverse('vote-list') + 'up/?model=movies&id='+str(self.movie.id)+'vote=true', content_type="application/json")
+
+        response = self.client.post(reverse('vote-list') + 'up/?model=event&id='+str(self.event.id)+'&vote=true', content_type="application/json")
         assert response.status_code == 200
         assert response.data['message'] == 'Successfully voted'
 
     def test_down(self):
         # user should be login to down vote
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('vote-list') + 'down/', {'model': 'movies', 'id': str(self.movie.id)})
+
+        response = self.client.get(reverse('vote-list') + 'down/', {'model': 'event', 'id': str(self.event.id)})
         assert response.status_code == 200
         assert response.data['message'] == 'Successfully un-voted'
 
     def test_down_post(self):
         # user should be login to down vote
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.post(reverse('vote-list') + 'down/?model=movies&id='+str(self.movie.id), content_type="application/json")
+
+        response = self.client.post(reverse('vote-list') + 'down/?model=event&id='+str(self.event.id), content_type="application/json")
         assert response.status_code == 200
         assert response.data['message'] == 'Successfully un-voted'
 
     def test_exists(self):
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('vote-list') + 'exists/', {'model': 'movies', 'id': str(self.movie.id)})
+
+        response = self.client.get(reverse('vote-list') + 'exists/', {'model': 'event', 'id': str(self.event.id)})
         assert response.status_code == 200
         assert response.data
 
     def test_all(self):
-        self.client.login(username=self.username, password=self.password)
+
 
         # check after up-vote
-        self.client.get(reverse('vote-list') + 'up/?model=movies&id='+str(self.movie.id)+'&vote=true')
-        response = self.client.get(reverse('vote-list') + 'all/?model=movies&id='+str(self.movie.id))
+        self.client.get(reverse('vote-list') + 'up/?model=event&id='+str(self.event.id)+'&vote=true')
+        response = self.client.get(reverse('vote-list') + 'all/?model=event&id='+str(self.event.id))
         assert response.status_code == 200
         assert response.data
 
         # check after down-vote
-        self.client.get(reverse('vote-list') + 'down/', {'model': 'movies', 'id': str(self.movie.id)})
-        response = self.client.get(reverse('vote-list') + 'all/', {'model': 'movies', 'id': str(self.movie.id)})
+        self.client.get(reverse('vote-list') + 'down/', {'model': 'event', 'id': str(self.event.id)})
+        response = self.client.get(reverse('vote-list') + 'all/', {'model': 'event', 'id': str(self.event.id)})
         assert response.status_code == 200
         assert len(response.data) == 0
 
     def test_count(self):
-        self.client.login(username=self.username, password=self.password)
+
 
         # check before vote
-        response = self.client.get(reverse('vote-list') + 'count/', {'model': 'movies', 'id': str(self.movie.id)})
+        response = self.client.get(reverse('vote-list') + 'count/', {'model': 'event', 'id': str(self.event.id)})
         assert response.status_code == 200
         self.assertDictEqual(response.data, {'vote_count': 0})
 
         # check after vote
-        self.client.get(reverse('vote-list') + 'up/', {'model': 'movies', 'id': str(self.movie.id), 'vote': 'true'})
-        response = self.client.get(reverse('vote-list') + 'count/', {'model': 'movies', 'id': str(self.movie.id)})
+        self.client.get(reverse('vote-list') + 'up/', {'model': 'event', 'id': str(self.event.id), 'vote': 'true'})
+        response = self.client.get(reverse('vote-list') + 'count/', {'model': 'event', 'id': str(self.event.id)})
         assert response.status_code == 200
         self.assertDictEqual(response.data, {'vote_count': 1})
 
     def test_users(self):
-        response = self.client.get(reverse('vote-list') + 'users/', {'model': 'movies', 'id': str(self.movie.id)})
+        response = self.client.get(reverse('vote-list') + 'users/', {'model': 'event', 'id': str(self.event.id)})
         assert response.status_code == 200
 
     def test_likes(self):
-        response = self.client.get(reverse('vote-list') + 'likes/?model=movies&id='+str(self.movie.id))
+        response = self.client.get(reverse('vote-list') + 'likes/?model=event&id='+str(self.event.id))
         assert response.status_code == 200
